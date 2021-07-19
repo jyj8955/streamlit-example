bf25fd558aebf5b4850937320e44eadc2fbcfa6d
jyj8955 <87632691+jyj8955@users.noreply.github.com>
2021-07-19 17:47:53 +0900

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
-
-db=pd.read_excel('user_cvrg_20210719.xls', header=1)
-sb.countplot(data=db, x = '유입채널(마감)' )
-
