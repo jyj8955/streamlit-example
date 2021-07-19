98d1ceb9dbb2fab7955f731c1f2ce77529b4a6c6
jyj8955 <87632691+jyj8955@users.noreply.github.com>
2021-07-19 17:47:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-
-
 db=pd.read_excel('user_cvrg_20210719.xls', header=1)
 sb.countplot(data=db, x = '유입채널(마감)' )
-plt.show()
+
